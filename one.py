1cc40bb4c50ee3b386906b337305d4a5fedaa4df
Piyush Singh <piyushsingh1982004@gmail.com>
2026-06-18 23:59:14 +0530

    first commit

---
+++ one.py
@@ -0,0 +1,11 @@
+a=int(input("Print the first no A:" ))
+b=int(input("Print the first no B:" ))
+op=input("operation that you want to perform: ")
+if op=="+":
+    print(a+b)
+elif op=="-":
+    print(a-b)
+elif op=="*":
+    print(a*b)
+elif op=="/":
+    print(a/b)
